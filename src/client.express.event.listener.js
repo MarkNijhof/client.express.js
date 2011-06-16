ca250528c18d9311e58d25f984377651b0e2eeb2
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-06-16 02:03:53 +0200

    removed some commented out code

--- src/client.express.event.listener.js
+++ src/client.express.event.listener.js
@@ -9,7 +9,6 @@ ClientExpress.EventListener = (function(server) {
   var eventListener = EventListener.prototype;
   
   eventListener.registerEventHandlers = function() {
-    console.log("Register Event Handlers");
 
     document.onclick = function() {
       var ev = arguments[0] || window.event;
@@ -25,7 +24,7 @@ ClientExpress.EventListener = (function(server) {
             window.location.pathname = element.href;
           }
         });
-        // Davis.history.pushState(request);
+
         _server.processRequest(request);
         return false;
       }
@@ -48,35 +47,10 @@ ClientExpress.EventListener = (function(server) {
           }
         });
 
-        console.log(request);
-        // Davis.history.pushState(request);
         _server.processRequest(request);
         return false;
       }
     };
-
-
-    // var links = document.getElementsByTagName('a');
-    // for (var i = 0, length = links.length, link = links[i]; i < length; i++) {
-    //   if (link.addEventListener) {
-    //     link.addEventListener('click', clickHandler, false);
-    //     console.log("found: " + link.href);
-    //   } else if (link.attachEvent) {
-    //     link.attachEvent('onclick', clickHandler);
-    //     console.log("found: " + link.href);
-    //   }
-    // }
-    // 
-    // var forms = document.forms;
-    // for (var i = 0, length = forms.length, form = forms[i]; i < length; i++) {
-    //   if (form.addEventListener) {
-    //     form.addEventListener('sumbit', submitHandler, false);
-    //     console.log("found: " + form.action);
-    //   } else if (form.attachEvent) {
-    //     form.attachEvent('onsubmit', submitHandler);
-    //     console.log("found: " + form.action);
-    //   }
-    // }
     
     // if (window.addEventListener) {
     //   window.addEventListener('popstate', onPopStateHandler, false);
@@ -85,36 +59,6 @@ ClientExpress.EventListener = (function(server) {
     // }
     
   };
-
-  // var handler = function(targetExtractor) {
-  //   var request = new ClientExpress.Request(targetExtractor.call(this));
-  //   // Davis.history.pushState(request);
-  //   // return _server.processRequest(request);
-  // };
-  // 
-  // var clickHandler = handler(function () {
-  //   var self = this
-  // });
-  // 
-  // var submitHandler = handler(function () {
-  //   var extractFormParams = function (form) {
-  //     return ClientExpress.utils.map(form.serializeArray(), function (attr) {
-  //       return [attr.name, attr.value].join('=')
-  //     }).join('&')
-  //   }
-  // 
-  //   var self = this
-  // 
-  //   return {
-  //     method: this.method,
-  //     fullPath: [this.action, extractFormParams(this)].join("?"),
-  //     title: this.title,
-  //     // session: _server.session(),
-  //     delegateToServer: function () {
-  //       self.submit();
-  //     }
-  //   };
-  // });
 
   return EventListener;  
 })();
