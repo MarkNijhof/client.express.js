315d2958e04b0a6c0cc4ebebbee6d2694b9df10d
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-06-21 17:00:17 +0200

    added a few version management helper functions to the rake file

--- rakefile.rb
+++ rakefile.rb
@@ -20,3 +20,24 @@ desc 'Generate the documentation'
 task :docs do
   exec "dox --title 'Davis' src/davis.*.js > docs/index.html"
 end
+
+task :major do
+  version = File.read('VERSION').strip.split('.')
+  major = Integer(version[1]) + 1
+  
+  File.open('VERSION', 'w') {|f| f.write("#{major}.#{version[1]}.#{version[2]}") }
+end
+
+task :minor do
+  version = File.read('VERSION').strip.split('.')
+  minor = Integer(version[1]) + 1
+  
+  File.open('VERSION', 'w') {|f| f.write("#{version[0]}.#{minor}.#{version[2]}") }
+end
+
+task :patch do
+  version = File.read('VERSION').strip.split('.')
+  patch = Integer(version[2]) + 1
+  
+  File.open('VERSION', 'w') {|f| f.write("#{version[0]}.#{version[1]}.#{patch}") }
+end
