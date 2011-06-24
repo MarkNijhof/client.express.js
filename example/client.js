6735a687064ce8a8e077e8a5c1286676d3484af3
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-06-24 09:06:50 +0200

    added caching to the retreaval of templates

--- example/client.js
+++ example/client.js
@@ -46,15 +46,29 @@ server.listen();
 
 var template_engine = {
   compile: function(template_url, args) {
-    var template_html;
-    $.ajax({ 
-      type: "GET",
-      url: template_url,   
-      async: false,
-      success : function(text) {
-         template_html = text;
-      }
-    });
+    var that = this;
+    var template_html = that.cache.get(template_url);
+    if (template_html == '') {
+      $.ajax({ 
+        type: "GET",
+        url: template_url,   
+        async: false,
+        success : function(text) {
+           template_html = text;
+           that.cache.set(template_url, template_html);
+        }
+      });
+    }
     return require("ejs").render(template_html, { locals: args });
+  },
+  cache: {
+    get: function(path) {
+      return this.templates.hasOwnProperty(path) ?
+        this.templates[path] : '';
+    },
+    set: function(path, template) {
+      this.templates[path] = template;
+    },
+    templates: {}
   }
 };
