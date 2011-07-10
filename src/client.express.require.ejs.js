bf73b08e3902eca732fdcf2f8bb890a66ea6cbd1
Mark Nijhof <Mark.Nijhof@Gmail.com>
2011-07-10 02:03:53 +0200

    removind the jQuery dependency from the template loading logic as well

--- src/client.express.require.ejs.js
+++ src/client.express.require.ejs.js
@@ -3,20 +3,34 @@
 require.modules['ejs_original.js'] = require.modules['ejs.js'];
 
 require.register("ejs.js", function(module, exports, require) {
+  function createXMLHttp() {
+    if (typeof XMLHttpRequest != 'undefined') {
+      return new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+      var avers = ["Microsoft.XmlHttp", "MSXML2.XmlHttp", "MSXML2.XmlHttp.3.0", "MSXML2.XmlHttp.4.0", "MSXML2.XmlHttp.5.0"];
+      for (var i = avers.length -1; i >= 0; i--) {
+        try {
+          httpObj = new ActiveXObject(avers[i]);
+          return httpObj;
+        } catch(e) {}
+      }
+    }
+    throw new Error('XMLHttp (AJAX) not supported');
+  }
 
   exports.compile = function(template_url, args) {
     var that = this;
     var template_html = that.cache.get(template_url);
     if (template_html == '') {
-      $.ajax({ 
-        type: "GET",
-        url: template_url,   
-        async: false,
-        success : function(text) {
-           template_html = text;
-           that.cache.set(template_url, template_html);
+      var ajaxObj = createXMLHttp();
+      ajaxObj.open('GET', template_url, false);
+      ajaxObj.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          template_html = this.responseText;
+          that.cache.set(template_url, template_html);
         }
-      });
+      };
+      ajaxObj.send();
     }
     return require("ejs_original").render(template_html, { locals: args });
   };
