83f1a0389825162b4cef6396049e1152e20db276
Mark Nijhof <mark.nijhof@gmail.com>
2011-09-29 22:48:49 +0200

    fixed a bundle issue

--- src/client.express.js
+++ src/client.express.js
@@ -1,16 +1,3 @@
-
-/**
- * @depends client.express.server.js
- * @depends client.express.route.js
- * @depends client.express.router.js
- * @depends client.express.event.broker.js  
- * @depends client.express.event.listener.js
- * @depends client.express.request.js
- * @depends client.express.response.js
- * @depends client.express.logger.js
- * @depends client.express.utils.js
- * @depends client.express.require.js
- */
  
 ClientExpress = {};
 
